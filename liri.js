fa23f121bf6544f257b8a84c86a40e008ec54978
Josh Vasquez <Joshvasquez204@gmail.com>
2019-10-04 10:19:02 -0500

    made some fixes

--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
 var searchType = process.argv[2];
 var argumentString = process.argv.slice(3).join('+');
 
-// adding logging
+// logging
 const fs = require('fs');
 const moment = require('moment');
 var now = moment().format('MMMM Do YYYY, h:mm:ss a');
 
+//include the music.js to get Spotify data back
 var music = require('./music');
 var movies = require('./movies');
 var bands = require('./bands');
